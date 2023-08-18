084549cbb111b93c2c5383222e8914a6e658f611
nvdovin <narmunine@gmail.com>
2023-08-18 15:22:18 +0300

    Реализация записи в json

--- avito_parser/avito.py
+++ avito_parser/avito.py
@@ -1,6 +1,5 @@
 import time
 from bs4 import BeautifulSoup
-from fake_useragent import UserAgent
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -8,16 +7,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from PIL import Image
+from threading import Thread
+
 import os
 import json
 
 
+active_threads, finiched_threads = 0, 0
+
+
+
 class AvitoParser:
+    def __init__(self, main_url: str) -> None:
+        self.main_url = main_url
+
     def get_html_src(self, url: str, phone=False):
         print("[INFO] Creating of driver")
         ops = webdriver.ChromeOptions()
-        # ops.add_argument("--headless")
-        ops.add_argument("--log-level=3")
+        ops.add_argument("--headless")
+        ops.add_argument("--log-level=2")
         driver = webdriver.Chrome(ops)
 
         print("[INFO] Getting page content")
@@ -31,38 +40,46 @@ class AvitoParser:
         
         if phone is False:
             print("[INFO] Success.")
-            return html_src, None
         else:
-            print("[INFO] Advice to parce number")
-            button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div/div[3]/div[1]/div/div[2]/div[3]/div[2]/div[1]/div/div/div[2]/div[1]/div/div[2]/div[1]/div/div/div/div/button')))
-            button.click()
+            print("[INFO] Try to get number")
+            try:
+                button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div/div[3]/div[1]/div/div[2]/div[3]/div[2]/div[1]/div/div/div[2]/div[1]/div/div[2]/div[1]/div/div/div/div/button')))
+                button.click()
+            except:
+                print("[INFO] There is no number")
+
+            if not os.path.exists("avito_parser/screenshots"):
+                os.mkdir("avito_parser/screenshots")
+            time.sleep(5)
+            driver.save_screenshot("screenshots/screen.png")
+
+            img = Image.open("avito_parser/screenshots/screen.png")
+            img.crop((110, 120, 430, 170)).save("screenshots/phone_number.png")
+
+            print("[INFO] Screenshot was saved")
             
-            for i in range(10):
-                try:
-                    phone_number = driver.find_element(By.XPATH, '/html/body/div[2]/div[63]/div/div/div/div[1]/div[2]/img').text
-                    print(phone_number)
-                    return html_src, phone_number
-                except:
-                    time.sleep(1)
+            print("[INFO] Success.")
+        return html_src
 
-    def getting_urls(self) -> None:
-        with open("index.html", "r", encoding="utf-8") as html_src:
-            s = html_src.read()
-            soup = BeautifulSoup(s, "lxml")
-            card_list = soup.find("div", class_="items-items-kAJAg").find_all("div", class_="iva-item-root-_lk9K")
+    def getting_urls(self):
+        response = self.get_html_src(url=self.main_url)
 
-            print(f"[INFO] Finded {len(card_list)} items")
+        soup = BeautifulSoup(response, "lxml")
+        card_list = soup.find("div", class_="items-items-kAJAg").find_all("div", class_="iva-item-root-_lk9K")
 
-            urls_list = []
-            for card in card_list:
-                link = f'https://www.avito.ru{card.find("div", class_="iva-item-title-py3i_").find("a").get("href")}'
-                urls_list.append(link)
-            
-            if not os.path.exists("text_files"):
-                os.mkdir("text_files")
-            with open(f"text_files/urls_list.txt", "w") as file_:
-                for url in urls_list:
-                    file_.writelines(f"{url}\n")
+        print(f"[INFO] Finded {len(card_list)} items")
+
+        urls_list = []
+        for card in card_list:
+            link = f'https://www.avito.ru{card.find("div", class_="iva-item-title-py3i_").find("a").get("href")}'
+            urls_list.append(link)
+
+        if not os.path.exists("avito_parser/text_files"):
+            os.mkdir("avito_parser/text_files")
+
+        with open("avito_parser/text_files/urls_list.txt", "w", encoding="utf-8") as text_file:
+            for url in urls_list:
+                text_file.write(f"{url}\n")
 
     @staticmethod
     def str_to_num(str_):
@@ -72,26 +89,56 @@ class AvitoParser:
         for n in str_list:
             if n.isdigit():
                 main_num += n
-        return float(main_num)
+        try:
+            return float(main_num)
+        except:
+            return str_
 
     def parse_every_url(self, url: str):
-        req, number = self.get_html_src(url=url, phone=True)
-        # test case
-        # with open("index.html", "r", encoding="utf-8") as file_:
-        #     s = file_.read()
+        global active_threads
+        active_threads += 1
 
-        soup = BeautifulSoup(req, "lxml")
+        response = self.get_html_src(url=url, phone=True)
+        soup = BeautifulSoup(response, "lxml")
 
         title = soup.find("span", class_="title-info-title-text").text
         price = self.str_to_num(soup.find("span", class_="style-price-value-main-TIg6u").text)
         image = soup.find("div", class_="image-frame-wrapper-_NvbY").find("img").get("src")
         adress = soup.find("span", class_="style-item-address__string-wt61A").text
         description = soup.find("div", class_="style-item-description-html-qCwUL").text.strip("\n\n")
-        phone_number = number
 
-        print(phone_number)
+        total_data = {
+            f"{title}":
+                {
+            "price": price,
+            "image": image,
+            "adress": adress,
+            "description": description
+            }
+        }
+
+        if not os.path.isfile("avito_parser/text_files/json_data.json"):
+            with open("avito_parser/text_files/json_data.json", "w", encoding="utf-8") as json_file:
+                json.dumps(json_file, [total_data], indent=4, ensure_ascii=True)
+        else:
+            with open("avito_parser/text_files/json_data.json", "w", encoding="utf-8") as json_file:
+                data = json.load(json_file)
+                new_data = data.append(total_data)
+                json.dumps(json_file, new_data, indent=4, ensure_ascii=True)
+
+    def main(self):
+        global active_threads, finiched_threads
+
+        self.getting_urls()
+
+        with open("avito_parser/text_files/urls_list.txt", "r", encoding="utf-8") as urls_list:
+            urls = urls_list.readlines()
+            for url in urls:
+                while active_threads - finiched_threads < 10:
+                    Thread(self.parse_every_url, args=[url], daemon=True)
+                    finiched_threads += 1
 
 
 if __name__ == "__main__":
-    ap = AvitoParser()
-    ap.parse_every_url(url="https://www.avito.ru/saransk/tovary_dlya_kompyutera/materinskaya_plata_k50ij_rev_2.1_nerabochaya_3117919975")
+    ap = AvitoParser(main_url="https://www.avito.ru/mordoviya/mototsikly_i_mototehnika/mototsikly-ASgBAgICAUQ80k0?cd=1&f=ASgBAgECAUQ80k0BRcaaDBV7ImZyb20iOjAsInRvIjoyMDAwMH0")
+    ap.main()
