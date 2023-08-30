b28766d1613fe948f401432a542c0c5d86a4e8bb
nvdovin <narmunine@gmail.com>
2023-08-30 13:18:46 +0300

    Реализация многопоточности

--- avito_parser/avito.py
+++ avito_parser/avito.py
@@ -1,5 +1,5 @@
-import time
 from bs4 import BeautifulSoup
+import fake_useragent
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -11,22 +11,35 @@ from PIL import Image
 from threading import Thread
 
 import os
+import sqlite3
 import json
+import time
+import requests
 
 
-active_threads, finiched_threads = 0, 0
+active_threads, finished_threads = 0, 0
 
 
 
 class AvitoParser:
-    def __init__(self, main_url: str) -> None:
+    def __init__(self, main_url: str, headless=False) -> None:
         self.main_url = main_url
+        self.headless = headless
 
     def get_html_src(self, url: str, phone=False):
+        ua = fake_useragent.UserAgent().random
+
         print("[INFO] Creating of driver")
         ops = webdriver.ChromeOptions()
-        ops.add_argument("--headless")
-        ops.add_argument("--log-level=2")
+
+        if self.headless is True:
+            ops.add_argument("--headless")
+
+        ops.add_argument("--log-level=3")
+        # ops.add_argument(f"--user-agent={ua}")
+        ops.page_load_strategy = 'eager'
+        ops.add_argument("--disable-blink-features=AutomationControlled")
+
         driver = webdriver.Chrome(ops)
 
         print("[INFO] Getting page content")
@@ -34,9 +47,9 @@ class AvitoParser:
         driver.get(url=url)
 
         html_src = driver.page_source
-        with open("index.html", "w", encoding="utf-8") as file_html:
-            print("[INFO] Writing to .html file")
-            file_html.write(html_src)
+        # with open("index.html", "w", encoding="utf-8") as file_html:
+        #     print("[INFO] Writing to .html file")
+        #     file_html.write(html_src)
         
         if phone is False:
             print("[INFO] Success.")
@@ -95,50 +108,117 @@ class AvitoParser:
             return str_
 
     def parse_every_url(self, url: str):
-        global active_threads
+        """Здесь парсим каждую отдельную ссылку"""
+
+        if not os.path.exists("avito_parser/databases"):
+            os.mkdir("avito_parser/databases")
+        con = sqlite3.connect("avito_parser/databases/database.db")
+        cur = con.cursor()
+
+        global active_threads, finished_threads
         active_threads += 1
 
-        response = self.get_html_src(url=url, phone=True)
+        response = self.get_html_src(url=url, phone=False)
         soup = BeautifulSoup(response, "lxml")
 
-        title = soup.find("span", class_="title-info-title-text").text
-        price = self.str_to_num(soup.find("span", class_="style-price-value-main-TIg6u").text)
-        image = soup.find("div", class_="image-frame-wrapper-_NvbY").find("img").get("src")
-        adress = soup.find("span", class_="style-item-address__string-wt61A").text
-        description = soup.find("div", class_="style-item-description-html-qCwUL").text.strip("\n\n")
+        try:
+            title = soup.find("span", class_="title-info-title-text").text
+            price = self.str_to_num(soup.find("span", class_="style-price-value-main-TIg6u").text)
+            image_url = soup.find("div", class_="image-frame-wrapper-_NvbY").find("img").get("src")
+            adress = soup.find("span", class_="style-item-address__string-wt61A").text
 
-        total_data = {
-            f"{title}":
-                {
-            "price": price,
-            "image": image,
-            "adress": adress,
-            "description": description
-            }
-        }
+            try:
+                description = soup.find("div", class_="style-item-description-html-qCwUL").text.strip("\n\n")
+            except:
+                description = "Нет данных"
+            
+            print(f"""
 
-        if not os.path.isfile("avito_parser/text_files/json_data.json"):
-            with open("avito_parser/text_files/json_data.json", "w", encoding="utf-8") as json_file:
-                json.dumps(json_file, [total_data], indent=4, ensure_ascii=True)
-        else:
-            with open("avito_parser/text_files/json_data.json", "w", encoding="utf-8") as json_file:
-                data = json.load(json_file)
-                new_data = data.append(total_data)
-                json.dumps(json_file, new_data, indent=4, ensure_ascii=True)
+{title}
+{price}
+{image_url}
+{adress}
+{description}
+
+""")
+
+            cur.execute(f"""--sql
+                        CREATE TABLE IF NOT EXISTS avito_answer(
+                        title VARCHAR(30),
+                        current_price FLOAT,
+                        image_link TEXT,
+                        adress VARCHAR(30),
+                        url_link TEXT,
+                        descr VARCHAR(50)
+                        previous_price FLOAT NULL)
+    """)
+            already_used = cur.execute(f"""--sql
+                        SELECT url_link FROM avito_answer
+    """)
+            already_used_links = str(already_used.fetchall()).replace("'", "").replace("(", "").replace(",),", "").replace("[", "").replace("]", "").replace(",)", "").split()
+            if url not in already_used_links:
+                cur.execute(f"""--sql
+                            INSERT INTO avito_answer(
+                            title,
+                            current_price,
+                            image_link,
+                            adress,
+                            url_link,
+                            descr
+                            )
+                            VALUES(
+                            {title},
+                            {price},
+                            {image_url},
+                            {adress},
+                            {url},
+                            {description}
+                            )
+                            """)
+            else:
+                cur.execute(f"""--sql
+                            UPDATE avito_answer SET
+                            previous_price = current_price,
+                            current_price = {price}
+                            WHERE 
+                            url_link = {url}
+                            """)          
+            con.commit()
+            finished_threads += 1
+        except:
+            print("""
+[ERROR] Page hasn't been open
+                  """)
+            finished_threads += 1
+        
 
     def main(self):
-        global active_threads, finiched_threads
+        global active_threads, finished_threads
 
-        self.getting_urls()
+        # self.getting_urls()
 
         with open("avito_parser/text_files/urls_list.txt", "r", encoding="utf-8") as urls_list:
             urls = urls_list.readlines()
             for url in urls:
-                while active_threads - finiched_threads < 10:
-                    Thread(self.parse_every_url, args=[url], daemon=True)
-                    finiched_threads += 1
+                self.parse_every_url(url=url)
+            
+                # while True:
+                #     if active_threads - finished_threads < 10:
+                #         print
+                #         Thread(target=self.parse_every_url, args=[url], daemon=True).start()
+                #         print(f"{active_threads} - {finished_threads} = {active_threads - finished_threads}")
+                #         time.sleep(1)
+                #         break
+                #     elif active_threads == finished_threads:
+                #         break
+                #     else:
+                #         time.sleep(1)
+
+        print("[INFO] Program is cycle over")
 
 
 if __name__ == "__main__":
-    ap = AvitoParser(main_url="https://www.avito.ru/mordoviya/mototsikly_i_mototehnika/mototsikly-ASgBAgICAUQ80k0?cd=1&f=ASgBAgECAUQ80k0BRcaaDBV7ImZyb20iOjAsInRvIjoyMDAwMH0")
+    os.system("cls")
+    ap = AvitoParser(main_url="https://www.avito.ru/mordoviya/mototsikly_i_mototehnika/mototsikly-ASgBAgICAUQ80k0?cd=1&f=ASgBAgECAUQ80k0BRcaaDBV7ImZyb20iOjAsInRvIjoyMDAwMH0",
+                     headless=False)
     ap.main()
