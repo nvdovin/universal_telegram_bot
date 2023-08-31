ba8df1332eae052409a4139c383b81d33e3bd40f
nvdovin <narmunine@gmail.com>
2023-08-31 17:34:52 +0300

    Реализовано обновление БД

--- wb_parser/wildberries.py
+++ wb_parser/wildberries.py
@@ -25,13 +25,19 @@ class WildberriesParser:
         self.request = request
         self.req_name = transcription(request)
 
+
     def get_html_sourse(self):
         """ Здесь создаётся готовая, прогруженная html странца с исходным кодом. """
 
         print("Создаю драйвер для сбора ссылок")
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")
-        driver = webdriver.Chrome()
+        opt = Options()
+        opt.add_argument("--log-level=3")
+        opt.page_load_strategy = 'eager'
+        opt.add_argument("--disable-blink-features=AutomationControlled")
+        opt.add_experimental_option('excludeSwitches', ['enable-logging'])
+        driver =  webdriver.Chrome(options=opt)
+        driver.minimize_window()
+        opt.headless = True
         wait = WebDriverWait(driver, 5)
         driver.get("https://www.wildberries.ru/")
 
@@ -40,7 +46,7 @@ class WildberriesParser:
         #search.send_keys(self.request, Keys.ENTER)
         time.sleep(1)
         search.send_keys(self.request, Keys.ENTER)
-               
+
         time.sleep(5)
         action = ActionChains(driver=driver)
 
@@ -57,7 +63,8 @@ class WildberriesParser:
         driver.close()
 
         return html_code
-        
+
+
     def parse_prepared_page(self):
         """ В этой функции готовая прогруженная страница разбирется на ссылки, составляющие эту самую страницу """
 
@@ -70,15 +77,16 @@ class WildberriesParser:
             url = item.find("a", class_="product-card__link").get("href")
             all_urls.append(url)
         
-        if not os.path.exists("urls"):
-            os.mkdir("urls")
+        if not os.path.exists("wb_parser/urls"):
+            os.mkdir("wb_parser/urls")
 
-        with open(f"urls/{self.req_name}.txt", "w") as file:
+        with open(f"wb_parser/urls/{self.req_name}.txt", "w") as file:
             print(f"Производится записть в {self.req_name}.txt")
             for url in all_urls:
                 file.write(f"{url}\n")
         
         return all_urls
+
 
     @staticmethod
     def int_price(price):
@@ -92,6 +100,7 @@ class WildberriesParser:
                 total_data.append(number)
         return int("".join(total_data))
 
+
     def parse_and_save(self, url):
         """ Здесь алгоритм переходит по каждой из ссылок и парсит необходимые данные прямо в БД """
 
@@ -100,6 +109,10 @@ class WildberriesParser:
 
         opt = Options()
         opt.add_argument('--headless')
+        opt.add_argument("--log-level=3")
+        opt.page_load_strategy = 'eager'
+        opt.add_argument("--disable-blink-features=AutomationControlled")
+        opt.add_experimental_option('excludeSwitches', ['enable-logging'])
         driver =  webdriver.Chrome(options=opt)
         driver.get(url)
         time.sleep(3)
@@ -118,9 +131,10 @@ class WildberriesParser:
             except:
                 time.sleep(1)
 
-        if not os.path.exists("databases"):
-            os.mkdir("databases")
-        con = sqlite3.connect(f"databases/{self.req_name}.db")
+        if not os.path.exists("wb_parser/databases"):
+            os.mkdir("wb_parser/databases")
+
+        con = sqlite3.connect(f"wb_parser/databases/{self.req_name}.db")
         cur = con.cursor()
 
         cur.execute("""--sql
@@ -128,7 +142,7 @@ class WildberriesParser:
                         product_name varchar(30),
                         current_price float,
                         previously_price float NULL,
-                        descr varchar(100),
+                        descr varchar(60),
                         url_link varchar(50)
                     )
         """)
@@ -167,56 +181,73 @@ class WildberriesParser:
             con.commit()
         complited_threads += 1
 
+
     def make_urls_list(self):
         all_urls = []
-        with open(f"urls/{self.req_name}.txt", "r") as txt_file:
+        with open(f"wb_parser/urls/{self.req_name}.txt", "r") as txt_file:
             for url in txt_file:
                 all_urls.append(url.strip())
         return all_urls
 
-    def pull_urls_to_function(self):
+
+    def start_threads(self):
         """ Здесь формируются несколько потоков для парсинга url """
 
         all_urls = self.make_urls_list()
         current_thread = 0
+
         global complited_threads
         for url_line in all_urls:
             while True:
                 if current_thread - complited_threads < 10:
-                    Thread(target=self.parse_and_save, args=[url_line], daemon=True).start()
+                    Thread(target=self.parse_and_save, args=[url_line, "refresh"], daemon=True).start()
                     current_thread += 1
                     break
                 else:
                     time.sleep(1)
 
 
-print("""
-[+] Чтобы сформировать новый поисковой запрос: 
-search
-          
-[+] Просмотреть список запросов:
-requests
-          
-[+] Чтобы обновить существующую БД:
-watch
 
-[+] Для выхода:
-exit""")
+print("""
+[+] Чтобы сформировать новый поисковой запрос: search
+
+[+] Чтобы обновить существующую БД: refresh
+
+[+] Для выхода: exit
+""")
+
 while True:
-    command = input("Введите команду: ")
+    command = input("\nВведите команду: ")
     match command:
         case "search":
-            req = input("Ваш запрос: \n")
-            wb = WildberriesParser(req)
-            wb.parse_prepared_page()
-            wb.pull_urls_to_function()
+            req = input("\nВаш запрос: ")
+            search_wb_parser = WildberriesParser(req)
+            search_wb_parser.parse_prepared_page()
+            search_wb_parser.start_threads()
+
+        case "refresh":
+            databases = os.listdir("wb_parser/databases")
+            len_of_databases = len(databases) - 1
+
+            i = 0
+            for db in databases:
+                print(f"{i}. {db}")
+                i += 1
             
+            chosen_db = input("\nВыберите, какую базу данных исследовать: ")
+            flag = True
+            while flag:
+                int_db = int(chosen_db)
+                if 0 <= int_db and int_db <= len_of_databases:
+                    db_name = databases[int_db].split(".")[0]
+                    print(db_name)
 
-        case "request":
-            pass
+                    refresh_wb_parser = WildberriesParser(f"{db_name}")
+                    refresh_wb_parser.start_threads()
 
-        case "watch":
-            pass
+                    flag = False
+                else:
+                    chosen_db = int(input("Попробуйте еще раз!: "))
 
         case "exit":
             print("До новых встреч!")
